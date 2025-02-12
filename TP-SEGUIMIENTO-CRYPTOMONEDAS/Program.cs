b4b7d9faa3ab05e1d7c961f976cf83cb2c6bce81
Martin Fernandez <martindfernandez23@gmail.com>
2025-02-11 22:00:45 -0300

    Cambiando la cadena de conexion

--- TP-SEGUIMIENTO-CRYPTOMONEDAS/Program.cs
+++ TP-SEGUIMIENTO-CRYPTOMONEDAS/Program.cs
@@ -22,8 +22,14 @@ namespace TP_SEGUIMIENTO_CRYPTOMONEDAS
                 // Configuraci�n de opciones para el DbContext
                 var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
                 // Cadena de conexi�n a la base de datos
-                optionsBuilder.UseSqlServer("server=MARTIN\\SQLEXPRESS; database=CryptoApp; integrated security=true;TrustServerCertificate = True");
-                //optionsBuilder.UseSqlServer("server=LUCAS\\SQLEXPRESS; database=CryptoApp; integrated security=true;TrustServerCertificate = True");
+                try
+                {
+                    optionsBuilder.UseSqlServer("server=MARTIN\\SQLEXPRESS; database=CryptoApp; integrated security=true;TrustServerCertificate = True");
+                }
+                catch (Exception)
+                {
+                    optionsBuilder.UseSqlServer("server=LUCAS\\SQLEXPRESS; database=CryptoApp; integrated security=true;TrustServerCertificate = True");
+                }
 
                 // Crear el contexto pasando las opciones
                 using (var context = new AppDbContext(optionsBuilder.Options))
